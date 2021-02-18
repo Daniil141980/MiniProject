cd621e882456be2a4f4e36af6521b19506629517
tankyboy <ps_maxim@mail.ru>
2021-02-18 20:25:48 +0300

    map_modes

--- MainFile.py
+++ MainFile.py
@@ -4,13 +4,14 @@ import os
 
 LAT_STEP = 0.01
 LON_STEP = 0.023
-
+MAP_TYPES = ['map', 'sat', 'sat,skl']
 
 class MapParams:
     def __init__(self):
         self.lat = 55.729738
         self.lon = 37.664777
         self.zoom = 15
+        self.map_type = MAP_TYPES[0]
 
     def update(self, event):
         lonstep = self.get_lon_step()
@@ -27,6 +28,12 @@ class MapParams:
             self.lat -= latstep
         elif event.key == pygame.K_UP and self.lat + latstep <= 180:
             self.lat += latstep
+        elif event.key == pygame.K_1 and self.map_type != MAP_TYPES[0]:
+            self.map_type = MAP_TYPES[0]
+        elif event.key == pygame.K_2 and self.map_type != MAP_TYPES[1]:
+            self.map_type = MAP_TYPES[1]
+        elif event.key == pygame.K_3 and self.map_type != MAP_TYPES[2]:
+            self.map_type = MAP_TYPES[2]
 
     def get_lon_step(self):
         return LON_STEP * (2 ** (15 - self.zoom))
@@ -35,7 +42,7 @@ class MapParams:
         return LAT_STEP * (2 ** (15 - self.zoom))
 
     def get_search_params(self):
-        return f'll={self.lon},{self.lat}&z={self.zoom}'
+        return f'll={self.lon},{self.lat}&z={self.zoom}&l={self.map_type}'
 
 
 def main():
