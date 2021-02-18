6636f41fd6eb8b32c90a9e71216bffa2a5548ffc
Daniil <Daniil141980@yandex.ru>
2021-02-18 15:56:48 +0300

    Maps API part 2

--- MainFile.py
+++ MainFile.py
@@ -1,12 +1,40 @@
-from showMap import show_map
+from showMap import load_map
+import pygame
+import os
+
+
+class MapParams:
+    def __init__(self):
+        self.lat = 55.729738
+        self.lon = 37.664777
+        self.zoom = 10
+
+    def update(self, event):
+        if event.key == pygame.K_PAGEUP and self.zoom < 17:
+            self.zoom += 1
+        elif event.key == pygame.K_PAGEDOWN and self.zoom > 0:
+            self.zoom -= 1
+
+    def get_search_params(self):
+        return f'll={self.lon},{self.lat}&z={self.zoom}'
 
 
 def main():
-    lat = 55.729738
-    lon = 37.664777
-    z = 10
-    ll_z = f'll={lon},{lat}&z={z}'
-    show_map(ll_z)
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    map_object = MapParams()
+    map_file = None
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            break
+        elif event.type == pygame.KEYUP:
+            map_object.update(event)
+        map_file = load_map(map_object.get_search_params())
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    os.remove(map_file)
 
 
 if __name__ == '__main__':
--- showMap.py
+++ showMap.py
@@ -30,3 +30,24 @@ def show_map(ll_z=None, map_type='map', add_params=None):
         pass
     pygame.quit()
     os.remove(map_file)
+
+
+def load_map(ll_z=None, map_type='map', add_params=None):
+    server = 'http://static-maps.yandex.ru/1.x/'
+    if ll_z:
+        params = f'?{ll_z}&l={map_type}'
+    else:
+        params = f'?l={map_type}'
+    if add_params:
+        params += f'&{add_params}'
+    response = requests.get(server + params)
+    if not response:
+        raise Exception('Error!!!')
+    map_file = 'map.png'
+    try:
+        with open(map_file, 'wb') as file:
+            file.write(response.content)
+    except IOError as ex:
+        print(f'ERROR! {ex}')
+        sys.exit(1)
+    return map_file
