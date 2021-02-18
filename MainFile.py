bc3f5ce605035ff2639077a3e247af9fe6ebb012
Daniil <Daniil141980@yandex.ru>
2021-02-18 16:01:21 +0300

    Maps API part 3

--- MainFile.py
+++ MainFile.py
@@ -2,18 +2,37 @@ from showMap import load_map
 import pygame
 import os
 
+LAT_STEP = 0.01
+LON_STEP = 0.023
+
 
 class MapParams:
     def __init__(self):
         self.lat = 55.729738
         self.lon = 37.664777
-        self.zoom = 10
+        self.zoom = 15
 
     def update(self, event):
+        lonstep = self.get_lon_step()
+        latstep = self.get_lat_step()
         if event.key == pygame.K_PAGEUP and self.zoom < 17:
             self.zoom += 1
         elif event.key == pygame.K_PAGEDOWN and self.zoom > 0:
             self.zoom -= 1
+        elif event.key == pygame.K_LEFT and self.lon - lonstep >= -90:
+            self.lon -= LON_STEP * (2 ** (15 - self.zoom))
+        elif event.key == pygame.K_RIGHT and self.lon + lonstep <= 90:
+            self.lon += LON_STEP * (2 ** (15 - self.zoom))
+        elif event.key == pygame.K_DOWN and self.lat - latstep >= -180:
+            self.lat -= latstep
+        elif event.key == pygame.K_UP and self.lat + latstep <= 180:
+            self.lat += latstep
+
+    def get_lon_step(self):
+        return LON_STEP * (2 ** (15 - self.zoom))
+
+    def get_lat_step(self):
+        return LAT_STEP * (2 ** (15 - self.zoom))
 
     def get_search_params(self):
         return f'll={self.lon},{self.lat}&z={self.zoom}'
